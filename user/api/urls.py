5bf93f7e26873698c3385a858d10f292e38d9e6b
sanjay <smtptesting405@gmail.com>
2021-12-24 23:15:20 +0530

    checking

--- user/api/urls.py
+++ user/api/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('user/<int:pk>', UserDetails.as_view(), name='user_details'),
     path('user/', UserList.as_view(), name='user_list'),
 
+
     # JWT Part.
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
